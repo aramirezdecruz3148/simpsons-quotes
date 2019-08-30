bf77a0149158c5e66759c5428ebaae81048b33da
aramirezdecruz3148 <leigh3148@gmail.com>
2019-08-30 14:04:48 -0700

    created my service, decided to use the multiple quotes because I like to have a few things displayed on the page

---
+++ src/services/simpsonsApi.js
@@ -0,0 +1,15 @@
+export const getQuotes = (count = 5) => {
+  return fetch(`https://thesimpsonsquoteapi.glitch.me/quotes?count=${count}`)
+    .then(res => {
+      if(!res.okay) throw 'Unable to retrieve quote';
+
+      return res.json();
+    })
+    .then(quotes => {
+      return quotes.map(quote => ({
+        quote: quote.quote,
+        image: quote.image,
+        characterName: quote.character
+      }));
+    });
+};
