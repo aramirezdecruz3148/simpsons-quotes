d16973ddf745fdf24f9682d017951110916988f7
aramirezdecruz3148 <leigh3148@gmail.com>
2019-08-30 15:00:28 -0700

    created my Quotes component to render my Quote

---
+++ src/components/quotes/Quotes.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Quote from './Quote';
+
+function Quotes({ quotes }) {
+  const quoteList = quotes.map(({ characterImage, characterName, quote }) => (
+    <li key={quote}>
+      <Quote 
+        characterImage={characterImage}
+        characterName={characterName}
+        quote={quote}
+      />
+    </li>
+  ));
+  
+  return (
+    <ul>
+      {quoteList}
+    </ul>
+  );
+}
+
+Quotes.propTypes = {
+  quotes: PropTypes.arrayOf(PropTypes.shape({
+    characterName: PropTypes.string.isRequired,
+    characterImage: PropTypes.string.isRequired,
+    quote: PropTypes.string.isRequired
+  })).isRequired
+};
+
+export default Quotes;
