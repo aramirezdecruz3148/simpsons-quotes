f6de16c6863d6ed32475de4b922be9448d4822d7
aramirezdecruz3148 <leigh3148@gmail.com>
2019-08-30 14:08:45 -0700

    thought we were using thunk for the build, just refactored my store.js to use promiseMiddleware in its place

--- src/store.js
+++ src/store.js
@@ -1,13 +1,13 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import reducer from './reducers';
-import thunk from 'redux-thunk';
+import { promiseMiddleware } from 'promise-middleware-redux';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   reducer,
   composeEnhancers(applyMiddleware(
-    thunk
+    promiseMiddleware
   ))
 );  
 
